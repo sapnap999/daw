d6703e741ec4835b92b068306eb8dce84e77a4b0
sapnap999 <frezyr4@gmail.com>
2026-09-03 22:44:28 -0400

    Redesigned App layout with FL Studio-inspired UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import React from 'react'
 import Transport from './components/Transport'
 import Mixer from './components/Mixer'
-import Sequencer from './components/Sequencer'
+import Piano from './components/Piano'
+import Playlist from './components/Playlist'
 import './styles/app.css'
 
 export default function App() {
   return (
     <div className="app-container">
       <header className="app-header">
-        <h1>idk DAW</h1>
-        <p>Simple music production</p>
+        <div className="header-left">
+          <h1>idk DAW</h1>
+        </div>
+        <div className="header-center">
+          <Transport />
+        </div>
+        <div className="header-right">
+          <span className="version">v0.1.0</span>
+        </div>
       </header>
       
-      <div className="app-content">
-        <Transport />
-        <Sequencer />
-        <Mixer />
+      <div className="workspace">
+        <div className="left-panel">
+          <Mixer />
+        </div>
+        
+        <div className="center-panel">
+          <Piano />
+          <Playlist />
+        </div>
       </div>
     </div>
   )
